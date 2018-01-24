cbb1b54f21c2268726ccefe64360dc7cd08f8461
Augusto Zeponi <augusto.zeponi@benner.com.br>
2018-01-24 17:43:58 -0200

    #25 - Desenvolvendo o layout do carrinho de compras

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -22,12 +22,6 @@ class CarrinhoController extends Controller
             'user_id' => Auth::id()
             ])->get();
 
-        dd([
-            $pedidos,
-            $pedidos[0]->pedido_produtos,
-            $pedidos[0]->pedido_produtos[0]->produto
-            ]);
-
         return view('carrinho.index', compact('pedidos'));
     }
 }
